62d8f108c45c49f9db0e3692c41905056f877152
ScarlettBlaize <cutiepie.ats@gmail.com>
2025-11-26 22:40:49 -0500

    half of a6

--- Kambaz/Users/dao.js
+++ Kambaz/Users/dao.js
@@ -1,30 +1,36 @@
 import { v4 as uuidv4 } from "uuid";
+import model from "./model.js";
 
-export default function UsersDao(db) {
-  let { users } = db;
+export default function UsersDao() {
 
   const createUser = (user) => {
     const newUser = { ...user, _id: uuidv4() };
-    users = [...users, newUser];
-    return newUser;
+    return model.create(newUser);
   };
 
-  const findAllUsers = () => users;
-  const findUserById = (userId) => users.find((user) => user._id === userId);
+  const findAllUsers = () => model.find();
+  const findUserById = (userId) => model.findById(userId);
   const findUserByUsername = (username) =>
-    users.find((user) => user.username === username);
-  
+    model.findOne({ username: username });
+
   const findUserByCredentials = (username, password) =>
-    users.find(
-      (user) => user.username === username && user.password === password
-    );
-  
-    const updateUser = (userId, user) =>
-    (users = users.map((u) => (u._id === userId ? user : u)));
-  
-    const deleteUser = (userId) =>
-    (users = users.filter((u) => u._id !== userId));
-    
+    model.findOne({ username, password });
+
+  const findUsersByRole = (role) => model.find({ role: role });
+
+  const updateUser = (userId, user) =>
+    model.updateOne({ _id: userId }, { $set: user });
+
+  //const deleteUser = (userId) => model.deleteOne({ _id: userId });
+  const deleteUser = (userId) => model.findByIdAndDelete(userId);
+
+  const findUsersByPartialName = (partialName) => {
+    const regex = new RegExp(partialName, "i"); // 'i' makes it case-insensitive
+    return model.find({
+      $or: [{ firstName: { $regex: regex } }, { lastName: { $regex: regex } }],
+    });
+  };
+
   return {
     createUser,
     findAllUsers,
@@ -33,5 +39,7 @@ export default function UsersDao(db) {
     findUserByCredentials,
     updateUser,
     deleteUser,
+    findUsersByRole,
+    findUsersByPartialName,
   };
 }
